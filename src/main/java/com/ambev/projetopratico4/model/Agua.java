bfe1ae02f53fcd7e20da45b39e6866b665deb2fa
Karen <karen.kubo@ab-inbev.com>
2023-11-16 17:41:03 -0300

    feat: add models and configs

---
+++ src/main/java/com/ambev/projetopratico4/model/Agua.java
@@ -0,0 +1,17 @@
+package com.ambev.projetopratico4.model;
+
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "aguas")
+public class Agua extends Produto{
+
+    private boolean temGas;
+
+    public boolean isTemGas() {
+        return temGas;
+    }
+
+    public void setTemGas(boolean temGas) {
+        this.temGas = temGas;
+    }
+}
---
+++ src/main/java/com/ambev/projetopratico4/repository/ProdutoRepository.java
@@ -0,0 +1,11 @@
+package com.ambev.projetopratico4.repository;
+
+import com.ambev.techempowers.model.Produto;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.List;
+
+public interface ProdutoRepository extends MongoRepository<Produto, String> {
+    List<Produto> findByNome(String nome);
+    // Você pode adicionar métodos personalizados de consulta, se necessário
+}
